3f1bf6c1728e13e9fa53f2b2c97145b45dc44304
Tynahan <nusilite@gmail.com>
2025-12-23 13:06:08 +0900

    feat(tree): add tree route

--- src/config/env.ts
+++ src/config/env.ts
@@ -15,3 +15,4 @@ export const MONKEYTYPE_USER_NAME = checkEnvVar(process.env.MONKEYTYPE_USER_NAME
 export const GITHUB_FINE_GRAINED_TOKEN = checkEnvVar(process.env.GITHUB_FINE_GRAINED_TOKEN, "GITHUB_FINE_GRAINED_TOKEN")
 export const GITHUB_OWNER = checkEnvVar(process.env.GITHUB_OWNER, "GITHUB_OWNER")
 export const GITHUB_REPO = checkEnvVar(process.env.GITHUB_REPO, "GITHUB_REPO")
+export const GITHUB_TREE_SHA = checkEnvVar(process.env.GITHUB_TREE_SHA, "GITHUB_TREE_SHA")
--- src/routers/githubRouter/githubRouter.ts
+++ src/routers/githubRouter/githubRouter.ts
@@ -1,10 +1,41 @@
-import { GITHUB_OWNER, GITHUB_REPO } from "@/config/env.js"
+import { GITHUB_OWNER, GITHUB_REPO, GITHUB_TREE_SHA } from "@/config/env.js"
 import octokit from "@/config/github.js"
 import { Router } from "express"
 
 const githubRouter = Router()
 
+// NOTE: 깃헙이랑 연결이 되었는지를 확인하기 위한 테스트용 api
 githubRouter.get("/", async (req, res) => {
+    try {
+        const response = await octokit.rest.repos.getContent({
+            owner: GITHUB_OWNER,
+            repo: GITHUB_REPO,
+            path: "src/tiny-knowledge/readonly-vs-Readonly.md",
+            mediaType: {
+                format: "raw",
+            },
+        })
+
+        res.status(200).json({ message: "---- healthy", ...response.data })
+    } catch (error) {
+        res.status(500).json({ message: "github failed", error })
+    }
+})
+
+githubRouter.get("/tree", async (req, res) => {
+    try {
+        const response = await octokit.rest.git.getTree({
+            owner: GITHUB_OWNER,
+            repo: GITHUB_REPO,
+            tree_sha: GITHUB_TREE_SHA,
+        })
+        res.status(200).json(response.data)
+    } catch (error) {
+        res.status(500).json({ message: "github failed", error })
+    }
+})
+
+githubRouter.get("/markdown/specific-example", async (req, res) => {
     try {
         const response = await octokit.rest.repos.getContent({
             owner: GITHUB_OWNER,
